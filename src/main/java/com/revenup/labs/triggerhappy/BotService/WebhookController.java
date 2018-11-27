b956fc26e5ffa862c0f9696f403e784733f7bdc9
tirumudi <tirumudi@gmail.com>
2018-11-27 20:30:00 +0530

    app commit

--- src/main/java/com/revenup/labs/triggerhappy/BotService/WebhookController.java
+++ src/main/java/com/revenup/labs/triggerhappy/BotService/WebhookController.java
@@ -5,13 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.df.webhook.Request;
 import com.google.df.webhook.response.DialogFlowResponse;
 import com.google.df.webhook.response.Message;
 import com.google.df.webhook.response.Text;
@@ -21,7 +22,7 @@ import com.google.df.webhook.response.Text;
 public class WebhookController {
 
 	@PostMapping(produces = "application/json")
-	public DialogFlowResponse processWebhook(HttpServletRequest request, HttpServletResponse response) {
+	public DialogFlowResponse processWebhook(@RequestBody Request request, HttpServletResponse response) {
 		DialogFlowResponse dialogFlowResponse = new DialogFlowResponse();
 		Text text = new Text(new String[] { "Test Response For the activity" });
 		Map<String, Message> fulfillmentMessage = new HashMap<>();
