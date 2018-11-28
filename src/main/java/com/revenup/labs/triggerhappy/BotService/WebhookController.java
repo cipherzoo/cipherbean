7eed40f89e3199c3dee581e4e4f17588c9a0e293
tirumudi <tirumudi@gmail.com>
2018-11-28 17:13:26 +0530

    app commit

--- src/main/java/com/revenup/labs/triggerhappy/BotService/WebhookController.java
+++ src/main/java/com/revenup/labs/triggerhappy/BotService/WebhookController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +17,29 @@ import com.google.df.webhook.Request;
 import com.google.df.webhook.response.DialogFlowResponse;
 import com.google.df.webhook.response.Message;
 import com.google.df.webhook.response.Text;
+import com.revenup.labs.triggerhappy.service.IntentProcessingService;
 
 @RestController
 @RequestMapping("/")
 public class WebhookController {
 
+	@Autowired
+	private IntentProcessingService intentProcessor;
+
 	@PostMapping(produces = "application/json")
 	public DialogFlowResponse processWebhook(@RequestBody Request request, HttpServletResponse response) {
 		String intent = request.getQueryResult().getIntent().getDisplayName();
-		DialogFlowResponse dialogFlowResponse = new DialogFlowResponse();
-		Text text = new Text(new String[] { "Test Response For the activity" });
 		Map<String, Message> fulfillmentMessage = new HashMap<>();
-		fulfillmentMessage.put("text", text);
+		switch (intent) {
+		case "get_insights":
+			break;
+		case "get_campaign_type_from_user":
+			fulfillmentMessage = intentProcessor.getCampaignsByCampaignType(request);
+			break;
+		default:
+			break;
+		}
+		DialogFlowResponse dialogFlowResponse = new DialogFlowResponse();
 		List<Map<String, Message>> fulfillementMessages = new ArrayList<>(1);
 		fulfillementMessages.add(fulfillmentMessage);
 		dialogFlowResponse.setFulfillmentMessages(fulfillementMessages);
--- src/main/java/com/revenup/labs/triggerhappy/service/IntentProcessingService.java
+++ src/main/java/com/revenup/labs/triggerhappy/service/IntentProcessingService.java
@@ -1,11 +1,45 @@
 package com.revenup.labs.triggerhappy.service;
 
-import com.google.df.webhook.response.Message;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+import com.google.df.webhook.Request;
+import com.google.df.webhook.response.Message;
+import com.google.df.webhook.response.Text;
+
+@Service
 public class IntentProcessingService {
 	public Message getInsights() {
 		return null;
 	}
-	
-	//public Message 
+
+	public Map<String,Message> getCampaignsByCampaignType(Request req) {
+		Map<String, String> parameters = req.getQueryResult().getParameters();
+		String queryText = req.getQueryResult().getQueryText();
+		StringBuilder builder = new StringBuilder();
+		String campaignType = parameters.containsKey("camapaign_types") ? parameters.get("campaign_types") : "";
+		// TODO
+		switch (campaignType) {
+		case "product based":
+			builder.append(
+					"Here are some cherry picked product based campaigns for you. Just enter the corresponding number to go ahead creating the campaign.")
+					.append("\n 1. Cross-sell Fixed Deposit, 2. Cross-sell Personal Loan, 3. Cross-sell Credit Card");
+			break;
+		case "goal based":
+			break;
+		case "treatment based":
+			break;
+		default:
+			break;
+		}
+		Text text = new Text(new String[] { builder.toString() });
+		Map<String, Message> fulfillmentMessage = new HashMap<>();
+		fulfillmentMessage.put("text", text);
+
+		return fulfillmentMessage;
+	}
+
+	// public Message
 }
