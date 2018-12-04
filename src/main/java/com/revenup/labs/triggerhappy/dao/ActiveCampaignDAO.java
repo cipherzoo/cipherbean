2c5e2b27658d4431a1962072644030febc04babc
tirumudi <tirumudi@gmail.com>
2018-12-04 15:22:05 +0530

    Application commit

--- src/main/java/com/revenup/labs/triggerhappy/dao/ActiveCampaignDAO.java
+++ src/main/java/com/revenup/labs/triggerhappy/dao/ActiveCampaignDAO.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ActiveCampaignDAO {
 
 	@Autowired
